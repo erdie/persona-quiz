0f6c1eafa890508b2062e215627276e911ea4b3f
Erdiawan Anna <erdie.one@gmail.com>
2026-08-14 13:30:53 +0700

    feat(nuxt.config): replace tailwindcss module with Vite plugin integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     css: [
         '~/assets/css/global.css'
     ],
-    modules: ['@nuxtjs/tailwindcss'],
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ],
+    },
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true }
 })
